02933e481356ad12492d47638cdc983e5fb42c6b
Daniel Komolafe <komozy2000@gmail.com>
2021-11-10 21:07:57 +0100

    implements registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,45 @@
-export const register = (req, res) => {
-    console.log(req.body);
+import User from '../models/user';
+import { errorHandler } from '../utils/dbErrorHandler';
+import { hashPassword, comparePassword } from '../utils/auth';
 
-    res.json("register user response from controller!");
+// Use async and await because the hashPassword function returns a promise
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        /* Validation of received values */
+        if (!name) return res.status(400).send("Name is required!");
+        if (!password) return res.status(400).send("Password is required!");
+        if (password && password.length < 6) {
+            return res.status(400).send("Password should be at least 6 characters long.");
+        }
+
+        // Check if email is already in the database
+        let userExists = await User.findOne({ email }).exec();
+        if (userExists) return res.status(400).send("Email is taken!");
+        /*------ End of validation -----*/
+
+        // Hash the password
+        const hashedPassword = await hashPassword(password);
+
+        // Save the user in the database
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await user.save((err, user) => {
+            if (err) {
+                return res.status(401).json({ error: errorHandler(err) });
+            }
+
+            return res.json({ message: "Signup success!" });
+        });
+
+    } catch (err) {
+        console.log(err);
+
+        res.status(400).send("Error! Please try again.");
+    }
 }
