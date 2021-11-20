105d1f375074909d7c60f9a140ebbf054ae0f0da
Daniel Komolafe <komozy2000@gmail.com>
2021-11-20 17:05:47 +0100

    become instructor with stripe onboarding account

---
+++ controllers/instructor.js
@@ -0,0 +1,61 @@
+import User from "../models/user";
+import queryString from "query-string";
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+export const makeInstructor = async (req, res) => {
+	try {
+		// Query database for user information
+		const user = await User.findById(req.user._id).exec();
+
+		// If user does not have stripe_account_id yet, create new account
+		// Pre-fill any info such as email (optional)
+
+		if (!user.stripe_account_id) {
+			const account = await stripe.accounts.create({ type: "express", "stripe_user[email]": user.email });
+
+			user.stripe_account_id = account.id;
+
+			user.save();
+		}
+
+		// Create account link based on account id (for frontend to complete onboarding)
+		let accountLink = await stripe.accountLinks.create({
+			account: user.stripe_account_id,
+			refresh_url: process.env.STRIPE_REDIRECT_URL,
+			return_url: process.env.STRIPE_REDIRECT_URL,
+			type: "account_onboarding",
+		});
+
+		// Then send the account link as response to frontend
+		res.send(`${accountLink.url}?${queryString.stringify(accountLink)}`);
+	} catch (error) {
+		console.log("MAKE INSTRUCTOR ERROR: ", error);
+	}
+};
+
+export const getAccountStatus = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id).exec();
+
+		const account = await stripe.accounts.retrieve(user.stripe_account_id);
+
+		if (!account.charges_enabled) {
+			return res.staus(401).send("Unauthorized!");
+		} else {
+			const statusUpdated = await User.findByIdAndUpdate(
+				user._id,
+				{
+					stripe_seller: account,
+					$addToSet: { role: "Instructor" },
+				},
+				{ new: true }
+			)
+				.select("-password")
+				.exec();
+
+			res.json(statusUpdated);
+		}
+	} catch (err) {
+		console.log(err);
+	}
+};
