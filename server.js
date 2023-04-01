bccc966f5391beff996605fa4822f9f063e3afbb
Daniel Komolafe <komozy2000@gmail.com>
2023-04-01 17:32:56 +0100

    added default route and sample .env

--- server.js
+++ server.js
@@ -21,25 +21,32 @@ app.use(morgan("dev"));
 
 // database
 const uri =
-	process.env.NODE_ENV === "production"
-		? process.env.DATABASE_CLOUD
-		: process.env.DATABASE;
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_CLOUD
+    : process.env.DATABASE;
 
 mongoose
-	.connect(uri)
-	.then(() => console.log("DB connected"))
-	.catch((err) => console.log("Error from DB: ", err));
+  .connect(uri)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("Error from DB: ", err));
 
 // routes
 readdirSync("./routes").map((fileName) => {
-	app.use("/api", require(`./routes/${fileName}`));
+  app.use("/api", require(`./routes/${fileName}`));
 });
 
 // csrf
 app.use(csrfProtection);
 
 app.get("/api/csrf-token", (req, res) => {
-	res.json({ csrfToken: req.csrfToken() });
+  res.json({ csrfToken: req.csrfToken() });
+});
+
+// default route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Scholarly API. All routes begin with /api.",
+  });
 });
 
 // port
@@ -47,5 +54,5 @@ const port = process.env.PORT || 8000;
 
 // start listening on port
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
